cfef4d98ca16473629133429a1c87176a23fe443
Luanderson Almeida <ls_luanderson@hotmail.com>
2024-11-17 19:30:25 +0000

    adicionando scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se você está lendo isso, você é uma pessoa especial demais!!</h1>
+  return <h1>Se você está lendo isso, você é uma pessoa especial demais!!</h1>;
 }
 
 export default Home;
