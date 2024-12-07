4e34cd35d846117d0788bff0b0a648739a71af50
Luanderson Almeida <ls_luanderson@hotmail.com>
2024-12-07 02:30:55 +0000

    adicionando jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
+  console.log(database);
   const result = await database.query("SELECT 1 + 1 as sum;");
   response.status(200).json({ chave: "Testes response!" });
   console.log(result.rows);
