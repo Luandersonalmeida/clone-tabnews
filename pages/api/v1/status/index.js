dc0d84933bd4102a65404988dc621e174f0d796a
Luanderson Almeida <ls_luanderson@hotmail.com>
2025-01-13 22:52:58 +0000

    adicionando o retorno da versao do postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,13 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  console.log(database);
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  response.status(200).json({ chave: "Testes response!" });
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion.rows[0].server_version,
+  });
+  console.log("teste", postgresVersion.rows[0].server_version);
 }
 
 export default status;
