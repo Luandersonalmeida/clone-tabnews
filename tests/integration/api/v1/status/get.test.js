f4c47e8504ef6f89db3f97ac6ca9cb68ab7438af
Luanderson Almeida <luanderson.santos@cwi.com.br>
2025-03-15 15:16:32 -0300

    add  GitHubActions correcao arquivos

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,9 +1,8 @@
-import orchestrator from "tests/orchestrator.js"
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-})
-
+});
 
 test("GET /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
