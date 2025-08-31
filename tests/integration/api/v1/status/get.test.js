232a309656550dad680b94bcbae5afe494283160
Luanderson Almeida <ls_luanderson@hotmail.com>
2025-08-31 16:01:37 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,11 +4,15 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET /api/v1/status deve retornar 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
